d63cc8b64b30666cc6f2c5ee934a7f8e9ed83aa3
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-06-12 11:42:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let result =  confirm("Do you want to proceed?")
+
+if(result==true){
+	alert("You clicked OK. Proceeding...")
+}
+else{
+	alert("You clicked Cancel.Exiting...")
+}
+// if (confirm("You clicked OK. proceeding...") == true) {
+			// 	alert("You clicked OK. Proceeding...") 
+			// } else {
+			// 	alert("You clicked Cancel. Exiting...");
+			// }
